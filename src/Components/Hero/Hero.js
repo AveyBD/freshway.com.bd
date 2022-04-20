327904e21ef49b2ba7710b8e4cfeee1aea59bea5
Debashish D. Dev <hello@debashish.dev>
2022-04-20 13:51:18 +0600

    Added Hero Section

---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="relative w-full h-full pb-10">
+        <div className="relative px-4 xl:px-0 container mx-auto md:flex items-center gap-8">
+          <div className="text-color w-full md:w-1/3 pt-16 lg:pt-32 xl:pt-12">
+            <h1 className="text-4xl md:text-4xl lg:text-6xl w-11/12 lg:w-11/12 xl:w-full xl:text-6xl text-gray-900 font-extrabold f-f-l">
+              Revolutionizing the Fresh Produce Supply Chain
+            </h1>
+            <div className="f-f-r text-base lg:text-base pb-20 sm:pb-0 pt-10 xl:pt-6">
+              <h2>
+              Driven by cutting edge technology, we source fresh produce from farmers and sell to businesses, across Bangladesh.
+              </h2>
+            </div>
+            <div>
+              <button className="hidden md:block hover:opacity-90 text-base w-full xl:text-base xl:w-6/12 mt-4 xl:mt-8 f-f-r py-4  bg-green-700 text-white font-bold focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 rounded-lg">
+                Learn More
+              </button>
+            </div>
+          </div>
+          <img
+            className="w-full mt-8 md:mt-0 object-fill md:w-2/3 md:-ml-4 lg:-ml-4 xl:ml-0"
+            src="https://tuk-cdn.s3.amazonaws.com/can-uploader/Graphic.png"
+            alt="sample page"
+            role="img"
+          />
+          <button className="md:hidden hover:opacity-90 text-base w-full xl:text-base xl:w-6/12 mt-4 xl:mt-8 f-f-r py-4  bg-indigo-700 text-white font-bold focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 rounded-lg">
+            Start building now
+          </button>
+          <button className="md:hidden hover:opacity-90 text-base w-full xl:text-base xl:w-4/12 xl:ml-2 mt-4 xl:mt-8 f-f-r py-4  bg-indigo-200 text-indigo-600 font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 rounded-lg">
+            Try it out
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Hero from '../Hero/Hero';
 
 const Home = () => {
     return (
         <div>
-            <h2>Hello Home</h2>
+           <Hero></Hero>
+
         </div>
     );
 };
