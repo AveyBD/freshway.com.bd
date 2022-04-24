23d68620e0756cb84da2391849f1be7848649683
Debashish D. Dev <hello@debashish.dev>
2022-04-24 11:36:19 +0600

    Added Key

--- src/Components/Opportunity/Opportunities/Opportunities.js
+++ src/Components/Opportunity/Opportunities/Opportunities.js
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import Opportunity from '../Opportunity/Opportunity';
+import React, { useEffect, useState } from "react";
+import Opportunity from "../Opportunity/Opportunity";
 
 const Opportunities = () => {
-    const [oppo, setOppo] = useState([]);
+  const [oppo, setOppo] = useState([]);
 
-    useEffect(()=>{
-        fetch('opportunities.json')
-        .then(res=> res.json())
-        .then(data => setOppo(data))
-    },[])
+  useEffect(() => {
+    fetch("opportunities.json")
+      .then((res) => res.json())
+      .then((data) => setOppo(data));
+  }, []);
 
-    return (
-        <div>
-            {
-                oppo.map(op=>(<Opportunity></Opportunity>))
-            }
-        </div>
-    );
+  return (
+    <div>
+      {oppo.map((op) => (
+        <Opportunity key={op.id} op={oppo}></Opportunity>
+      ))}
+    </div>
+  );
 };
 
 export default Opportunities;
