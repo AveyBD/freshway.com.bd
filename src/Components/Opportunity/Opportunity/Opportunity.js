88a2f2e9f3eccd740714d1f79a5b448a8447fc76
Debashish D. Dev <hello@debashish.dev>
2022-04-24 12:44:24 +0600

    Added Benifits

--- src/Components/Opportunity/Opportunity/Opportunity.js
+++ src/Components/Opportunity/Opportunity/Opportunity.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import { FcInfo } from "react-icons/fc";
+import React from "react";
 
-const Opportunity = ({op}) => {
-    console.log(op);
-    return (
-        <div className='border bg-green-100 p-2 rounded'>
-            <img src={op.img} alt={op.name} />
-            <h2 className='font-bold text-2xl mt-1 '>{op.name}</h2>
-            
-        </div>
-    );
+const Opportunity = ({ op }) => {
+  console.log(op.Benefits);
+  return (
+    <div className="border bg-green-100 hover:bg-green-400 hover:scale-110 duration-300 ease-linear p-2 rounded hover:drop-shadow-xl mb-10">
+      <img src={op.img} alt={op.name} />
+      <h2 className="font-bold text-2xl mt-1 ">{op.name}</h2>
+      <ul>
+        <li className="flex gap-1 items-center ">
+            <FcInfo></FcInfo>
+          {op.Benefits[0]}
+        </li>
+        <li className="flex gap-1 items-center ">
+            <FcInfo></FcInfo>{op.Benefits[1]}</li>
+            <li className="flex gap-1 items-center ">
+            <FcInfo></FcInfo>{op.Benefits[2]}</li>
+            <li className="flex gap-1 items-center ">
+            <FcInfo></FcInfo>{op.Benefits[3]}</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Opportunity;
