dbc5cc09790e87731ec9712eeae5e216a5b9a6b3
Debashish D. Dev <hello@debashish.dev>
2022-07-18 17:56:27 +0600

    Added About Page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -14,15 +14,20 @@ const About = () => {
           <p className="font-normal text-sm leading-3 text-indigo-700 hover:text-indigo-800 cursor-pointer">
             About
           </p>
-          <h2 className="xl:w-8/12 lg:w-10/12 w-full font-bold text-gray-800 lg:text-4xl text-3xl lg:leading-10 leading-9 mt-2">
-            We are here to make fresh product for everyone
+          <h2 className="xl:w-8/12 lg:w-10/12 w-full font-bold text-gray-800 lg:text-3xl text-3xl lg:leading-10 leading-9 mt-2">
+            Bangladesh 1ST Hi-Tech agricultural park
           </h2>
           <p className="font-normal text-base leading-6 text-gray-600 mt-6">
-            We connect producers of food directly with retailers, restaurants,
-            and service providers using in-house applications that drive end to
-            end operations. Currently, our Supply Chain is equipped to move 1400
-            tonnes of perishables from farms to businesses, every day, in less
-            than 12 hours.
+            Agriculture is a predominant sector in Bangladesh in generating
+            livelihood, employment, and GDP contribution. Although agriculture
+            remains the third most contributing sector to the country’s GDP, its
+            contribution has decreased over the past decade going from 17% In
+            2010 to 12.6% in 2020. The industry is also a significant
+            contributor to export earnings and provider of raw material sources
+            for other sectors, such as poultry and livestock feed, leather,
+            frozen food, canned food, etc. It is the core of the Bangladeshi
+            economy, playing an essential role in reducing poverty and achieving
+            food security in the past.
           </p>
         </div>
 
@@ -47,25 +52,18 @@ const About = () => {
         <div className="lg:mt-16 sm:mt-12 mt-16 flex lg:flex-row justify-between flex-col lg:gap-8 gap-12">
           <div className="w-full xl:w-5/12 lg:w-6/12">
             <h2 className="font-bold lg:text-4xl text-3xl lg:leading-9 leading-7 text-gray-800">
-              Our Story
+              Our Aim
             </h2>
             <p className="font-normal text-base leading-6 text-gray-600 mt-4">
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum.In the first place we have granted to
-              God, and by this our present charter confirmed for us and our
-              heirs forever that the English Church shall be free, and shall
-              have her rights entire, and her liberties inviolate; and we will
-              that it be thus observed; which is apparent from
+              To ensure a sustainable growth going forward, the industry needs
+              to move towards high-tech farming and value chain development to
+              reduce costs, increase production, and achieve better product
+              quality.
             </p>
             <p className="font-normal text-base leading-6 text-gray-600 mt-6">
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum.In the first place we have granted to
-              God, and by this our present charter confirmed for us and our
-              heirs forever that the English Church shall be free, and shall
-              have her rights entire, and her liberties inviolate; and we will
-              that it be thus observed; which is apparent from
+              Post harvesting losses of annually stand at $2.38 bn per year (Tk
+              30,000 crores) The Business Standard a loss that can be salvaged
+              through agro-processing, packaging, storage and proper logistics.
             </p>
           </div>
           <div className="lg:flex items-center w-full lg:w-1/2 ">
